d25665373e4c5f8166c4e1f35ac28eb2b9acd55d
LESTER CERIOLI <lcerioli@lucastecnologiaservices.onmicrosoft.com>
2020-12-26 18:16:57 -0300

    Implementing a simple product

---
+++ Video2-2/Class22/Class22/Client/Classes/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Class22.Client.Classes
+{
+    public class Calculator
+    {
+        public static int Some(int n1, int n2) => n1 + n2;
+        public static int Subtract(int n1, int n2) => n1 - n2;
+        public static int Multiply(int n1, int n2) => n1 * n2;
+    }
+}
---
+++ Video2-2/Class22/Class22/Shared/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class22.Shared.Entities
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public DateTime RegisterDate { get; set; }
+    }
+}
